3b5e82fac42885f5db73c2155e4b7bde5cc18ce9
sparkesmitchell <sparkesmitchell@gmail.com>
2026-03-20 12:19:10 -0700

    Launch Version

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,6 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
